26830ac5f094e2e47ee01dea04e207bf556f2b01
James <54754876+jamesstats@users.noreply.github.com>
2022-04-14 18:04:22 -0400

    Update polling.R

--- polling.R
+++ polling.R
@@ -7,3 +7,4 @@ url <-  "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2022_French_presi
 url_html <-read_html(url)
 url_tables <-url_html%>%html_table(fill=TRUE) 
 results<-url_tables[[9]]
+write_csv(results,paste0('data/',Sys.Date(),'_macron_lePen','.csv'))    
